23b905f5efabcb5351d95fb2da1004c636a0e382
BlankRip <aniketh_g5@outlook.com>
2024-05-12 09:54:48 +0530

    Aiming in direction of the mouse

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -2,18 +2,22 @@ extends CharacterBody2D
 
 
 @export var speed: float = 300.0
+@export var shootLineLength: float = 60.0
+@export var shootDirectionLine: Line2D
 var stoppingSpeed: float = 0
+const lineEndPointIndex: int = 1
 
 func _ready():
 	stoppingSpeed = speed/4
 
-
 func _physics_process(delta):
-	# var direction = Input.get_axis("ui_left", "ui_right")
 	var direction = Vector2(Input.get_axis("move_left", "move_right"), Input.get_axis("move_down", "move_up"))
 	if direction:
 		velocity = direction * speed
 	else:
 		velocity.x = move_toward(velocity.x, 0, stoppingSpeed)
 		velocity.y = move_toward(velocity.y, 0, stoppingSpeed)
+	var directionToMouse = (get_global_mouse_position() - shootDirectionLine.global_position).normalized()
+	shootDirectionLine.points[lineEndPointIndex] = directionToMouse * shootLineLength
+	
 	move_and_slide()
