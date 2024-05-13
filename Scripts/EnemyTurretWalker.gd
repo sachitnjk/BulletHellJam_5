10079f29ff86772e2153ed97444ecd81187c95b5
Sachit Nanajkar <sachitnjk@gmail.com>
2024-05-13 20:07:53 +0100

    EnemyTurretWalker radial DirectionRotator added

--- Scripts/EnemyTurretWalker.gd
+++ Scripts/EnemyTurretWalker.gd
@@ -1,8 +1,9 @@
 extends "res://Scripts/EnemyBase.gd"
 
 @export var bulletSpeed : float
-@export var shootInterval : float
-@export var bulletSpawnPoint: Transform2D
+@export_range(0.1, 2) var shootInterval : float
+@export_range(0.1, 5) var rotationIncrementRate : float
+#@export var bulletSpawnPoint: Transform2D
 
 var playerNode
 var targetPosition
@@ -10,13 +11,14 @@ var shootTimer : float = 0.0
 var isMoving : bool = false
 var canMove : bool = true
 var canShoot : bool = true
+var baseFireDirection : Vector2 = Vector2(1, 0)
 
 func _ready():
 	var rootNode = get_tree().get_root().get_child(0)
 	playerNode = rootNode.get_node("Player")
 
 func _physics_process(delta):
-	
+	DirectionRotator()
 	MoveToRandomPoint()
 	if isMoving:
 		var direction = (targetPosition - global_position).normalized()
@@ -28,7 +30,7 @@ func _physics_process(delta):
 		if canShoot:
 			shootTimer += delta
 			if shootTimer >= shootInterval:
-				FireBullet((playerNode.global_position - global_position).normalized(), global_position)
+				FireBullet(baseFireDirection, global_position)
 				shootTimer = 0.0
 	pass
 
@@ -38,6 +40,10 @@ func MoveToRandomPoint():
 		targetPosition = playerNode.transform.origin + randomOffset
 		
 		isMoving = true
+
+func DirectionRotator():
+	var rotationAngle = rotationIncrementRate * get_physics_process_delta_time()
+	baseFireDirection = baseFireDirection.rotated(rotationAngle)
 
 func CheckDistanceToTargetPos():
 	if global_position.distance_to(targetPosition) <= 100:
