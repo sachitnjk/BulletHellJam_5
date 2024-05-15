d1da78f884fddfad0690dbab169c95f291a4525c
Sachit Nanajkar <sachitnjk@gmail.com>
2024-05-15 10:28:58 +0100

    Added Button UI sound

---
+++ Scripts/UISound.gd
@@ -0,0 +1,13 @@
+extends AudioStreamPlayer
+
+func _on_play_button_down():
+	play()
+
+func _on_exit_button_down():
+	play()
+
+func _on_return_to_menu_button_down():
+	play()
+
+func _on_play_again_button_down():
+	play()
